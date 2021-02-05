e3bbb16869beef3de8f4ef3e94627ec5600549fa
Sven Emtell <sven.emtell@doremir.com>
2021-02-05 15:01:01 +0100

    Add suffix '.js' to local imports.

--- remus.js
+++ remus.js
@@ -3,32 +3,32 @@ import Fraction from 'fraction.js';
 
 import Store from './lib/store.js';
 
-import AudioFile from './lib/classes/audio-file';
-import Duration from './lib/classes/duration';
-import Harmony from './lib/classes/harmony';
-import Interval from './lib/classes/interval';
-import Item from './lib/classes/item';
-import Pitch from './lib/classes/pitch';
-import MusicPtr from './lib/music-ptr';
+import AudioFile from './lib/classes/audio-file.js';
+import Duration from './lib/classes/duration.js';
+import Harmony from './lib/classes/harmony.js';
+import Interval from './lib/classes/interval.js';
+import Item from './lib/classes/item.js';
+import Pitch from './lib/classes/pitch.js';
+import MusicPtr from './lib/music-ptr.js';
 
-import Audio from './lib/classes/event/audio';
-import Chord from './lib/classes/event/chord';
-import ChordSequence from './lib/classes/event/chord-sequence';
-import Clef from './lib/classes/meta/clef';
-import Mode from './lib/classes/mode';
-import Note from './lib/classes/event/note';
-import NoteChord from './lib/classes/event/note-chord';
-import Rest from './lib/classes/event/rest';
-import Song from './lib/classes/event/song';
-import VerticalContainer from './lib/classes/event/vertical-container';
-import Voice from './lib/classes/event/voice';
-import VoiceContainer from './lib/classes/event/voice-container';
-import Tempo from './lib/classes/meta/tempo';
-import Time from './lib/classes/meta/time';
-import Key from './lib/classes/meta/key';
-import Tuplet from './lib/classes/event/tuplet';
+import Audio from './lib/classes/event/audio.js';
+import Chord from './lib/classes/event/chord.js';
+import ChordSequence from './lib/classes/event/chord-sequence.js';
+import Clef from './lib/classes/meta/clef.js';
+import Mode from './lib/classes/mode.js';
+import Note from './lib/classes/event/note.js';
+import NoteChord from './lib/classes/event/note-chord.js';
+import Rest from './lib/classes/event/rest.js';
+import Song from './lib/classes/event/song.js';
+import VerticalContainer from './lib/classes/event/vertical-container.js';
+import Voice from './lib/classes/event/voice.js';
+import VoiceContainer from './lib/classes/event/voice-container.js';
+import Tempo from './lib/classes/meta/tempo.js';
+import Time from './lib/classes/meta/time.js';
+import Key from './lib/classes/meta/key.js';
+import Tuplet from './lib/classes/event/tuplet.js';
 
-import Score from './lib/score/score';
+import Score from './lib/score/score.js';
 
 import NoteSplit from 'note-split';
 
